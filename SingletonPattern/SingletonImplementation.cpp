164dbb36167ab31dd8e33120740b081fa1a50e04
Hilmi cihan <se.hilmiyildirim@gmail.com>
2024-05-08 15:31:19 +0000

    SingletonFirst

--- SingletonPattern/SingletonImplementation.cpp
+++ SingletonPattern/SingletonImplementation.cpp
@@ -1,62 +1,35 @@
 #include <iostream>
+#include <string>
+using namespace  std;
+class Singleton 
+{
+    private:
+        static Singleton* instance;
+        string data;
+        Singleton(const string& value) : data(value) {}
+        Singleton(const Singleton&) = delete;
+        Singleton& operator = (const Singleton&) = delete;
+    public: 
+        static  Singleton* getInstance(const string& value)
+        {
+            if(instance == nullptr)
+            {
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
+        // Function display data
+        void showData() const {
+            cout << "The Data is: " << data<< endl;
+        }
 
-class Singleton {
-public:
-	// Static method to access the singleton instance
-	static Singleton& getInstance()
-	{
-		// If the instance doesn't exist, create it
-		if (!instance) {
-			instance = new Singleton();
-		}
-		return *instance;
-	}
 
-	// Public method to perform some operation
-	void someOperation()
-	{
-		std::cout
-			<< "Singleton is performing some operation."
-			<< std::endl;
-	}
-
-	// Delete the copy constructor and assignment operator
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-
-private:
-	// Private constructor to prevent external instantiation
-	Singleton()
-	{
-		std::cout << "Singleton instance created."
-				<< std::endl;
-	}
-
-	// Private destructor to prevent external deletion
-	~Singleton()
-	{
-		std::cout << "Singleton instance destroyed."
-				<< std::endl;
-	}
-
-	// Private static instance variable
-	static Singleton* instance;
 };
-
-// Initialize the static instance variable to nullptr
-Singleton* Singleton::instance = nullptr;
-
+// Singleton* Singleton::instance = nullptr;
 int main()
 {
-	// Access the Singleton instance
-	Singleton& singleton = Singleton::getInstance();
-
-	// Use the Singleton instance
-	singleton.someOperation();
-
-	// Attempting to create another instance will not work
-	// Singleton anotherInstance; // This line would not
-	// compile
-
-	return 0;
+    Singleton* s1 = Singleton::getInstance("Singleton data");
+    s1->showData();
+    Singleton* s2 =Singleton::getInstance("Another Data");
+    s2->showData();
 }
