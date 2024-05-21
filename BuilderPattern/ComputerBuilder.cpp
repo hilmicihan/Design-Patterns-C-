f166f248fccfce7916d1493aeb7c2f94a5098ccb
Hilmi Cihan Yıldırım <hilmi.cihan.ext@arceurope.com>
2024-05-21 09:35:37 +0300

    builder

--- BuilderPattern/ComputerBuilder.cpp
+++ BuilderPattern/ComputerBuilder.cpp
@@ -1,63 +1,57 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+using namespace std;
 class Computer {
-public:
-    std::string cpu;
-    std::string ram;
-    std::string storage;
-
+    public:
+    string cpu;
+    string ram;
+    string storage;
     void specifications() const {
-        std::cout << "Computer Specifications:" << std::endl;
-        std::cout << "CPU: " << cpu << std::endl;
-        std::cout << "RAM: " << ram << std::endl;
-        std::cout << "Storage: " << storage << std::endl;
+        cout<<"Computer Specifications: "<<endl;
+        cout<<"CPU: "<<cpu<<endl;
+        cout<<"RAM: "<<ram<<endl;
+        cout<<"Storage: "<<storage<<endl;
+
     }
 };
 class ComputerBuilder {
-public:
+    public:
     virtual ~ComputerBuilder() = default;
-    virtual ComputerBuilder& setCPU(const std::string& cpuType) = 0;
-    virtual ComputerBuilder& setRAM(const std::string& ramType) = 0;
-    virtual ComputerBuilder& setStorage(const std::string& storageType) = 0;
-    virtual Computer* getComputer() = 0;
+    virtual ComputerBuilder& setCPU(const string& cpuType) =0;
+    virtual ComputerBuilder& setRAM(const string& ramType) =0;
+    virtual ComputerBuilder& setStorage(const string& storageType) =0;
+    virtual Computer* getComputer() =0;
 };
-
 class CustomComputerBuilder : public ComputerBuilder {
-private:
+    private:
     Computer* computer;
-
-public:
-    CustomComputerBuilder() { computer = new Computer(); }
-
-    ComputerBuilder& setCPU(const std::string& cpuType) override {
+    public:
+    CustomComputerBuilder() {
+        computer = new Computer();
+        }
+    ComputerBuilder& setCPU(const string& cpuType) override {
         computer->cpu = cpuType;
         return *this;
-    }
 
-    ComputerBuilder& setRAM(const std::string& ramType) override {
+    }
+    ComputerBuilder& setRAM(const string& ramType) override {
         computer->ram = ramType;
         return *this;
+        
     }
-
-    ComputerBuilder& setStorage(const std::string& storageType) override {
+    ComputerBuilder& setStorage(const string& storageType) override {
         computer->storage = storageType;
         return *this;
+        
     }
-
     Computer* getComputer() override {
         return computer;
     }
 };
 int main() {
     CustomComputerBuilder builder;
-
-    // Simulating dynamic configuration
-    builder.setCPU("AMD Ryzen 9").setRAM("64GB DDR4").setStorage("2TB NVMe SSD");
+    builder.setCPU("AMD Ryzen 9").setRAM("64gb DDR4").setStorage("2TB NVMe SSD");
     Computer* customComputer = builder.getComputer();
-      builder.setCPU("AMD Ryzen 3").setRAM("64GB DDR4").setStorage("2TB NVMe SSD");
-      Computer* customComputer2 = builder.getComputer();
     customComputer->specifications();
-     customComputer2->specifications();
-    return 0;
 }
